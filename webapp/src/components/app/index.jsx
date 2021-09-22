f7391ed7dbf6ba1d89c94565d8c4a1152aee3e53
Oliver Tworkowski <olitworkowski@gmail.com>
2021-09-22 02:45:44 +0200

    init

---
+++ webapp/src/components/app/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style";
+
+const App = () => {
+  return (
+    <div className="some">
+      test text
+      <div>test text</div>
+      <div>test text</div>
+      <div>test text</div>
+    </div>
+  );
+};
+
+export default App;
